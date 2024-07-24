a6a357963df9799f5158fa89914ac3ad121d4f11
a-nodi <yoonhyeok.lee@postech.ac.kr>
2024-07-24 19:59:11 +0900

    [Feat] Added zombie process killing function

--- multi-env/melee-env/melee_env/env.py
+++ multi-env/melee-env/melee_env/env.py
@@ -22,6 +22,19 @@ def find_available_udp_port(start_port: int = 1024, end_port: int = 65535) -> in
     raise OSError("no availiable port")
 
 
+def kill_zombies(runtime_to_kill: float = 1200):
+    """
+    kill any dolphin emulators that have been running for more than 20 minutes
+    """
+    for proc in psutil.process_iter():
+        try:
+            if "dolphin-emu" in proc.name() and time.time() - proc.create_time() > runtime_to_kill:
+                proc.kill()
+
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            print(f"zombie process kill failure, pid={proc.pid}")
+
+
 class MeleeEnv:
     def __init__(
         self,
